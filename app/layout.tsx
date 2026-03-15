20fc35e521a7ec385e24bc0b269e16f2f737fa25
Freeman1890 <ljt463015573@gmail.com>
2026-03-15 19:00:21 +0800

    新增统计代码

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -16,6 +17,21 @@ export default function RootLayout({
     <html lang="en">
       <body className="antialiased">
         {children}
+        <div style={{ display: "none" }}>
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-L1SQ5EQQ4Q"
+            strategy="lazyOnload"
+          />
+          <Script id="gtag-init" strategy="lazyOnload">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-L1SQ5EQQ4Q');
+            `}
+          </Script>
+        </div>
       </body>
     </html>
   );
